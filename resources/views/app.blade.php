4ed9054bd6b19f0fca27cf83154ebab450ea716f
Your Name <you@example.com>
2021-12-20 14:46:42 +0200

    start

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Make your cv">
+    <meta name="keywords" content="cv, curiculum, resume">
+    <title>Resume</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body class="">
+<?php
+$user = DB::table('users')->where('login', 'vasya')->first();
+
+echo $user->email;
+?>
+</body>
+</html>
